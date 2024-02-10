cbe8d328aafc14ca77de6136d1f95415a17878a5
dineshrock26 <dineshrock26@gmail.com>
2024-02-10 14:14:52 +0530

    nav

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -24,11 +24,11 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+    <nav className="fixed mx-auto border border-[#ffffff] top-0 left-0 right-0 z-10 bg-[#ffffff] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
-          className="text-2xl md:text-5xl text-white font-semibold"
+          className="text-2xl md:text-5xl text-gray-900 font-semibold"
         >
           SET
         </Link>
@@ -36,14 +36,14 @@ const Navbar = () => {
           {!navbarOpen ? (
             <button
               onClick={() => setNavbarOpen(true)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-gray-900 hover:text-gray-900 hover:border-gray-900"
             >
               <Bars3Icon className="h-5 w-5" />
             </button>
           ) : (
             <button
               onClick={() => setNavbarOpen(false)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-gray-900 hover:text-gray-900 hover:border-gray-900"
             >
               <XMarkIcon className="h-5 w-5" />
             </button>
