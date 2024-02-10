33903faf21014738aad62a5de685a2fd2abd8c09
dineshrock26 <dineshrock26@gmail.com>
2024-02-10 15:59:35 +0530

    hero

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,16 +15,16 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-gray-700 mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
+          <h1 className="text-gray-700 mb-4 text-3xl sm:text-4xl lg:text-6xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, Let&apos;s{" "}
             </span>
             <br></br>
             <TypeAnimation
               sequence={[
-                "Grow together !",
+                "Grow together!",
                 1000,
-                "make an Impact !",
+                "make an Impact!",
                 1000,
                 // "Mobile Developer",
                 // 1000,
